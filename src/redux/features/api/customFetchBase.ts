6017118ef4df17839dca4d58b87c9e740f484f66
Noormohammad011 <noormohammad.net1503011@gmail.com>
2023-07-18 23:18:23 +0600

    adding base url

--- src/redux/features/api/customFetchBase.ts
+++ src/redux/features/api/customFetchBase.ts
@@ -7,7 +7,8 @@ import {
 import { Mutex } from 'async-mutex';
 import { userLoggedOut } from '../auth/authSlice';
 
-const baseUrl = 'http://localhost:5000/api/v1/';
+const baseUrl = 'book-catalog-backend-lac.vercel.app/api/v1/api/v1/';
+
 
 // Create a new mutex
 const mutex = new Mutex();
