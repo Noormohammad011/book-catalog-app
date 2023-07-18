35ced3f4d167ddd14f58eb5af1688893237376fd
Noormohammad011 <noormohammad.net1503011@gmail.com>
2023-07-18 19:38:04 +0600

    removing unwanted component

--- src/pages/Books2.tsx
+++ src/pages/Books2.tsx
@@ -1,7 +1,5 @@
-import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react';
+import { Dialog, Disclosure, Transition } from '@headlessui/react';
 import {
-  ChevronDownIcon,
-  FunnelIcon,
   MinusIcon,
   PlusIcon,
 } from '@heroicons/react/20/solid';
@@ -15,17 +13,7 @@ import { useGetAllBooksQuery } from '../redux/features/books/bookApi';
 import { IBook, genreOptions } from '../types/globalTypes';
 import moment from 'moment';
 
-const sortOptions = [
-  { name: 'Any Time', href: '/', current: true },
-  { name: 'Past 24 Hours', href: '/', current: false },
-  { name: 'Past Month', href: '/', current: false },
-];
 
-const subCategories = [
-  { name: 'Mystery', href: '/' },
-  { name: 'Horror', href: '/' },
-  { name: 'Fantasy', href: '/' },
-];
 const filters = [
   {
     id: 'genre',
@@ -120,18 +108,6 @@ export default function Books() {
                   {/* Filters */}
                   <form className="mt-4 border-t border-gray-200">
                     <h3 className="sr-only">Categories</h3>
-                    <ul
-                      role="list"
-                      className="px-2 py-3 font-medium text-gray-900"
-                    >
-                      {subCategories.map((category) => (
-                        <li key={category.name}>
-                          <a href={category.href} className="block px-2 py-3">
-                            {category.name}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
 
                     {filters.map((section) => (
                       <Disclosure
@@ -202,62 +178,6 @@ export default function Books() {
             <h1 className=" text-2xl lg:text-4xl font-bold tracking-tight text-gray-900">
               Our Books
             </h1>
-
-            <div className="flex items-center">
-              <Menu as="div" className="relative inline-block text-left">
-                <div>
-                  <Menu.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-                    Sort
-                    <ChevronDownIcon
-                      className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                      aria-hidden="true"
-                    />
-                  </Menu.Button>
-                </div>
-
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <div className="py-1">
-                      {sortOptions.map((option) => (
-                        <Menu.Item key={option.name}>
-                          {({ active }) => (
-                            <a
-                              href={option.href}
-                              className={classNames(
-                                option.current
-                                  ? 'font-medium text-gray-900'
-                                  : 'text-gray-500',
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm'
-                              )}
-                            >
-                              {option.name}
-                            </a>
-                          )}
-                        </Menu.Item>
-                      ))}
-                    </div>
-                  </Menu.Items>
-                </Transition>
-              </Menu>
-
-              <button
-                type="button"
-                className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
-                onClick={() => setMobileFiltersOpen(true)}
-              >
-                <span className="sr-only">Filters</span>
-                <FunnelIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
-            </div>
           </div>
 
           <section aria-labelledby="products-heading" className="pb-24 pt-6">
@@ -274,20 +194,6 @@ export default function Books() {
                   searchTerm={searchTerm}
                   setSearchTerm={setSearchTerm}
                 />
-                {/* =============================================================================== */}
-                {/* =============================================================================== */}
-                <ul
-                  role="list"
-                  className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900"
-                >
-                  {subCategories.map((category) => (
-                    <li key={category.name}>
-                      <a href={category.href}>{category.name}</a>
-                    </li>
-                  ))}
-                </ul>
-                {/* =============================================================================== */}
-                {/* =============================================================================== */}
                 {filters.map((section) => (
                   <Disclosure
                     as="div"
