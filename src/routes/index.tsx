f4880d9ed3a3c4ca23d03f4110dc455016736864
Noormohammad011 <noormohammad.net1503011@gmail.com>
2023-07-18 21:54:20 +0600

    Filtaring is done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../layouts';
 import Home from '../pages/App';
 import BookDetails from '../pages/BookDetails';
-import Books from '../pages/Books2';
+import Books from '../pages/Books';
 import BookForm from '../pages/CreateBook';
 import ErrorPage from '../pages/ErrorPage';
 import Login from '../pages/Login';
